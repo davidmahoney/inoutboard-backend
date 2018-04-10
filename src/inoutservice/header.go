0df595d60c4de2196a34c6e2eb2483ac3d526c23
David Mahoney <clawmaster@gmail.com>
2018-04-10 13:40:28 -0700

    added cache-control header

--- src/inoutservice/header.go
+++ src/inoutservice/header.go
@@ -7,6 +7,7 @@ import (
 func AddHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
+		w.Header().Add("Cache-Control", "no-cache")
 		next.ServeHTTP(w, r)
 	})
 }
